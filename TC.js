378a75975c266f8ed532e41769bfcca4e76c2fb1
Etana <ansathlasta@gmail.com>
2011-10-20 16:18:43 +0200

    Adding first version of TC.js file

---
+++ TC.js
@@ -0,0 +1,20 @@
+var TC= {
+	tid				: function() {
+		return $("input[name=tid]:first").val()	
+	},
+	tpost			: function(p,e) {
+		/* en cours : code pour poster un sujet */
+	},
+	pdelete		: function(p,e) {
+		$.post("/post",{p:p,mode:"delete",confirm:""},e);
+	},
+	tdelete		: function(t,f,e) {
+		$.post("/modcp?tid="+TC.tid(),{f:f, t:t, tid: TC.tid(), mode:"delete",confirm:""},e);
+	},
+	trash			: function(t,e) {
+		$.get("/modcp?mode=trash&t="+t+"&tid="+TC.tid(),e)
+	},
+	move			: function(t,f,e) {
+		$.post("/modcp?tid="+TC.tid(),{tid:TC.tid(),new_forum:"f"+f,mode:"move",t:t,confirm:1},e);
+	},
+};
