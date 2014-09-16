8104f26dfbfb84ffff5a5da849facd8328e56886
ea <ea@gizal.org>
2014-09-16 23:08:06 +0200

    fix indent

--- FA.js
+++ FA.js
@@ -110,141 +110,93 @@ var $f = $f || {};
    */
   $f.charset = (document.charset ? document.charset : document.characterSet).toUpperCase();
 
-  /**
-   * $f.user_id - id of user
-   *
-   * Return: user id, -1 if guest
-   */
-  $f.user_id = _ud["user_id"];
 
-    /**
-     * $f.tid - user temporary identifier
-     *
-     * Return: tid or empty string
-     */
+  /**
+   * $f.tid - user temporary identifier
+   *
+   * Return: tid or empty string
+   */
   $f.tid = $("input[name=tid]:first").val() || ($("a[href*='&tid=']:first").attr("href") || "").replace(/^.*&tid=([a-z0-9]*)?.*$/, "$1");
 
-    /**
-     * $f.username - user username
-     *
-     * Return: username or Anonymous if guest
-     */
-  $f.username = _ud["username"];
+  $f.user_id = _ud["user_id"];          // id of user
+  $f.username = _ud["username"];        // user username (or Anonymous)
+  $f.is_guest = !_ud["session_logged_in"];// is user a guest?
+  $f.is_admin = _ud["user_level"] == 1; // is user an admin?
+  $f.is_mod = _ud["user_level"] > 0;    // is user a moderator?
+  $f.lang = _ud["lang"];                // user langage (fr for french, ...)
+  $f.avatar = _ud["avatar"];            // user avatar
+  $f.num_posts = _ud["user_posts"];     // user number of post
+  $f.num_pms = _ud["user_nb_privmsg"];  // user number of private message
+  $f.num_reps = _ud["point_reputation"];// user number of rep points
 
-    /**
-     * $f.is_guest - is user a guest?
-     *
-     * Return: true if guest, false otherweise
-     */
-  $f.is_guest = !_ud["session_logged_in"];
-
-    /**
-     * $f.is_admin - is user an admin?
-     *
-     * Return: true if admin, false otherweise
-     */
-  $f.is_admin = _ud["user_level"] == 1;
-
-    /**
-     * $f.is_mod - is user a moderator?
-     *
-     * Return: true if moderator or admin, false otherweise
-     */
-  $f.is_mod = _ud["user_level"] > 0;
-    
-    /**
-     * $f.lang - user interface language
-     *
-     * Return: langage code (fr for french, en for english, ...)
-     */
-  $f.lang = _ud["lang"];
-
-    /**
-     * $f.avatar - user avatar
-     *
-     * Return: langage code (fr for french, en for english, ...)
-     */
-  $f.avatar = _ud["avatar"];
-
-    /**
-     * $f.num_posts - user number of post
-     */
-  $f.num_posts = _ud["user_posts"];
-
-    /**
-     * $f.num_pms - user number of private message
-     */
-  $f.num_pms = _ud["user_nb_privmsg"];
-
-  /* $f.num_reps - user number point of reputation */
-  $f.num_reps = _ud["point_reputation"];
-
-    /**
-     * $f.send_pm - send a private message
-     *
-     * @username: array with recipients usernames
-     * @subject: subject
-     * @message: message content
-     * @callback: function called with sended form page as parameter
-     */
+  /**
+   * $f.send_pm - send a private message
+   *
+   * @username: array with recipients usernames
+   * @subject: subject
+   * @message: message content
+   * @callback: function called with sended form page as parameter
+   */
+  /* TODO ADD OTHER FIELDS */
   $f.send_pm = function(usernames, subject, message, callback) {
     $.post("/privmsg", _param({"username":username, subject:subject, message:message, mode:"post", post:1}), e);
   };
 
-    /**
-     * $f.get_post_data - get data fields of a message
-     * 
-     * @post_id: message id
-     * @callback: function called with data fields as parameters
-     */
+  /**
+   * $f.get_post_data - get data fields of a message
+   * 
+   * @post_id: message id
+   * @callback: function called with data fields as parameters
+   */
   $f.get_post_data = function(post_id, callback) {
     callback && $.get("/post?p=" + post_id + "&mode=editpost", function(p) {
       callback($('form[name="post"]', p).serializeArray());
     });
   };
 
-    /**
-     * $f.modify_post - modify a message
-     *
-     * @post_id: id of message
-     * @modifier: function which receive each data field and can modify them for a new value (or undefined to remove field)
-     */
+  /**
+   * $f.modify_post - modify a message
+   *
+   * @post_id: id of message
+   * @modifier: function which receive each data field and can modify them for a new value (or undefined to remove field)
+   */
   $f.modify_post = function(post_id, modifier) {
     $f.post.get_form(post_id, function(f) {
       $.post("/post", _param(f, modifier) + "&post=1");
     });
   };
 
-    /**
-     * $f.delete_post - delete a message
-     *
-     * @post_id: id of message
-     * @callback: function called with sended form page as parameter
-     */
+  /**
+   * $f.delete_post - delete a message
+   *
+   * @post_id: id of message
+   * @callback: function called with sended form page as parameter
+   */
   $f.delete_post = function(post_id, callback) {
     $.post("/post", {p:post_id, mode:"delete", confirm:""}, callback);
   };
 
-    /**
-     * $f.reply_topic - post a new reply to a topic
-     *
-     * @topic_id: topic id
-     * @message: message content
-     * @callback: function called with sended form page as parameter
-     */
+  /**
+   * $f.reply_topic - post a new reply to a topic
+   *
+   * @topic_id: topic id
+   * @message: message content
+   * @callback: function called with sended form page as parameter
+   */
+  /* TODO ADD OTHER FIELDS */
   $f.reply_topic = function(topic_id, message, callback) {
     $.post("/post", {subject:"", message:message, mode:"reply", t:topic_id, post:1, notify:0}, callback);
   };
 
-    /**
-     * $f.split_topic - split a topic
-     *
-     * @new_title: title of new topic
-     * @new_forum_id: forum id of new topic
-     * @posts_id: array with post to place in new topic
-     * @old_topic_id: id of topic in which messages are currently
-     * @callback: function called with sended form page as parameter
-     */
+  /**
+   * $f.split_topic - split a topic
+   *
+   * @new_title: title of new topic
+   * @new_forum_id: forum id of new topic
+   * @posts_id: array with post to place in new topic
+   * @old_topic_id: id of topic in which messages are currently
+   * @callback: function called with sended form page as parameter
+   */
   $f.split_topic = function(new_title, new_forum_id, posts_id, old_topic_id, callback) {
     if (typeof p != "object") {
       p = [p];
@@ -252,16 +204,16 @@ var $f = $f || {};
     $.post("/modcp?tid=" + $f.tid, {subject:new_title, new_forum_id:"f" + new_forum_id, split_type_all:1, post_id_list:posts_id, t:old_topic_id, mode:"split"}, callback);
   };
 
-    /* TODO combine split all and split beyond */
-    /**
-     * Diviser les messages à la suite d'un message donné dans un nouveau sujet.
-     * @param {String} title - titre du nouveau sujet.
-     * @param {Number} forum_id - identifiant du forum dans lequel le sujet sera créé.
-     * @param {Array} posts_id - tableau avec la liste des identiants de message à partir desquels on découpe.
-     * @param {Number} old_topic_id - identifiant du sujet dans lesquels les messages se trouvent.
-     * @param {Function} callback - fonction qui sera appelée avec en paramètre la page de résultat de division.
-     * @returns {undefinded}
-     */
+  /* TODO combine split all and split beyond */
+  /**
+   * Diviser les messages à la suite d'un message donné dans un nouveau sujet.
+   * @param {String} title - titre du nouveau sujet.
+   * @param {Number} forum_id - identifiant du forum dans lequel le sujet sera créé.
+   * @param {Array} posts_id - tableau avec la liste des identiants de message à partir desquels on découpe.
+   * @param {Number} old_topic_id - identifiant du sujet dans lesquels les messages se trouvent.
+   * @param {Function} callback - fonction qui sera appelée avec en paramètre la page de résultat de division.
+   * @returns {undefinded}
+   */
   $f.split_topic_beyond = function(title, forum_id, posts_id, old_topic_id, callback) {
     if (typeof p != "object") {
       p = [p];
@@ -269,22 +221,22 @@ var $f = $f || {};
     $.post("/modcp?tid=" + $f.tid, {subject:title, new_forum_id:"f" + forum_id, split_type_beyond:1, post_id_list:posts_id, t:old_topic_id, mode:"split"}, callback);
   };
 
-    /**
-     * $f.delete_topic - detete a topic
-     *
-     * @topic_id: id of topic
-     * @callback: function called with sended form page as parameter
-     */
+  /**
+   * $f.delete_topic - detete a topic
+   *
+   * @topic_id: id of topic
+   * @callback: function called with sended form page as parameter
+   */
   $f.delete_topic = function(topic_id, callback) {
     $.post("/modcp?tid=" + $f.tid, {t:topic_id, mode:"delete", confirm:1}, callback);
   };
 
-    /**
-     * $f.trash_topic - move a topic to trash
-     *
-     * @topic_id: id of topic
-     * @callback: function called with sended form page as parameter
-     */
+  /**
+   * $f.trash_topic - move a topic to trash
+   *
+   * @topic_id: id of topic
+   * @callback: function called with sended form page as parameter
+   */
   $f.trash_topic = function(topic_id, callback) {
     $.get("/modcp?mode=trash&t=" + topic_id + "&tid=" + $f.tid, callback);
   };
@@ -300,5 +252,6 @@ var $f = $f || {};
   $f.move_topic = function(topic_id, forum_id, callback) {
     $.post("/modcp?tid=" + $f.tid, {tid:$f.tid, newforum:"f" + forum_id, mode:"move", t:topic_id, confirm:1}, callback);
   };
+  /* TODO ADD CREATE TOPIC */
 })();
 
